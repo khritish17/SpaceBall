7144b60afe6fbeb2796aa57c2185e1cd3f734165
Khritish Kumar Behera <khritish.official@gmail.com>
2024-03-21 16:24:50 +0530

    Third Persone camera rig successfully completed

--- Space Ball/orange_character.gd
+++ Space Ball/orange_character.gd
@@ -1,14 +1,13 @@
 extends CharacterBody3D
 
 var forward_speed = 0
-var reverse_speed = 0
-var max_forward_speed = 10
+var max_forward_speed = 20
 var target_forward_speed = 0
-var target_reverse_speed = 0
 
 
 var pitch_input = 0
 var pitch_speed = 0.5
+var prev_pitch_val = 0
 
 var turn_input = 0
 var turn_speed = 0.75
@@ -19,8 +18,11 @@ var accleration = 9.8
 func get_inputs(delta):
 	# forward motion
 	
-	target_forward_speed = Input.get_action_strength("throtle_up") * max_forward_speed
-	target_reverse_speed = Input.get_action_strength("throtle_down") * max_forward_speed
+	#target_forward_speed = Input.get_action_strength("throtle_up") * max_forward_speed
+	#target_reverse_speed = Input.get_action_strength("throtle_down") * max_forward_speed
+	target_forward_speed = 0
+	target_forward_speed += Input.get_action_strength("throtle_up") * max_forward_speed
+	target_forward_speed -= Input.get_action_strength("throtle_down") * max_forward_speed
 	
 	pitch_input = 0
 	pitch_input -= Input.get_action_strength("pitch_up")
@@ -29,35 +31,25 @@ func get_inputs(delta):
 	turn_input = 0
 	turn_input += Input.get_action_strength("roll_left")
 	turn_input -= Input.get_action_strength("roll_right")
-	if Input.get_action_strength("roll_left"):
-		print("Roll LEFT")
-		print(Input.get_action_strength("roll_left"))
-	if Input.get_action_strength("roll_right"):
-		print("Roll RIGHT")
-		print(Input.get_action_strength("roll_right"))
 
 func _physics_process(delta):
 	get_inputs(delta)
 	
 	# forward and reverse motion
-	forward_speed = lerp(int(forward_speed), int(target_forward_speed), accleration*delta)
-	reverse_speed = lerp(int(reverse_speed), int(target_reverse_speed), accleration*delta)
-	if forward_speed:
-		velocity = transform.basis.z * forward_speed
-	else:
-		velocity.z = lerp(int(velocity.z), 0, accleration*delta)
-	if reverse_speed:
-		velocity = -transform.basis.z * reverse_speed
-	else:
-		velocity.z = lerp(int(velocity.z), 0, accleration*delta)
+	forward_speed = lerp(int(forward_speed), int(target_forward_speed), 0.2)
+	velocity = transform.basis.z * forward_speed
 	
-	# pitch action
-	transform.basis = transform.basis.rotated(transform.basis.x, pitch_input  * delta * pitch_speed)
+	# pitch action (node up or down motion while plane is climbing up or down)
+	prev_pitch_val = pitch_input  * delta * pitch_speed
+	transform.basis = transform.basis.rotated(transform.basis.x, prev_pitch_val)
+	#transform.basis = transform.basis.rotated(transform.basis.x, lerp(rotation.x, prev_pitch_val, level_speed * delta).normalized)
+	
 	# left and right turn
 	transform.basis = transform.basis.rotated(transform.basis.y, turn_input  * delta * turn_speed)
-	#transform.basis = transform.basis.rotated(transform.basis.z, turn_input  * delta * turn_speed)
+	
+	# roll motion  (banking of plane while turning left or right)
 	rotation.z = lerp(rotation.z, -turn_input * turn_speed, level_speed * delta)
-	#$Mesh/Body.rotation.y = lerp($Mesh/Body.rotation.y, turn_input, level_speed * delta)
+	
 	
 	
 		
---
+++ Space Ball/orange_ship_camera.gd
@@ -0,0 +1,26 @@
+extends Camera3D
+
+var target_node : NodePath = NodePath("..")
+var offset = Vector3.ZERO
+var space_ship
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_physics_process(false)
+	space_ship = get_node(target_node)
+	# calculating the offset
+	var spaceship_position = space_ship.global_position
+	var camera_position = global_position
+	offset = camera_position - spaceship_position
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# get the rotation of space_ship
+	var ship_position = space_ship.global_transform.origin
+	var desired_position = ship_position  + space_ship.global_transform.basis.x * offset.x + space_ship.global_transform.basis.y * offset.y + space_ship.global_transform.basis.z * offset.z
+	var smooth_position = lerp(global_transform.origin, desired_position, 0.1)
+	global_transform.origin = smooth_position
+	look_at(ship_position)
+	#print(rotations)
